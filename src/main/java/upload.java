54e2a9c084e00fc6da37973d0623a6b1ac962bf2
josep <joseph.townend@hotmail.com>
2024-01-18 10:56:39 +0000

    BFYUWAKHRGQI

--- src/main/java/upload.java
+++ src/main/java/upload.java
@@ -4,27 +4,30 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.List;
 
 public class upload {
-    public static void main(String[] args) {
-        String uploadUrl = "https://example.com/upload";
-        String filePath = "path/to/your/extracted/file.html";
+    public void main(String fileContent) {
+        Config c = new Config();
+        List<Site> s = c.getSites();
+
+        String uploadUrl = s.get(0).getName();
 
         try {
-            uploadFile(uploadUrl, filePath);
+            uploadFile(uploadUrl, fileContent);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void uploadFile(String uploadUrl, String filePath) throws IOException {
+    private static void uploadFile(String uploadUrl, String fileContent) throws IOException {
         URL url = new URL(uploadUrl);
         HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
         httpURLConnection.setDoOutput(true);
         httpURLConnection.setRequestMethod("POST");
 
         try (OutputStream outputStream = httpURLConnection.getOutputStream()) {
-            byte[] fileBytes = java.nio.file.Files.readAllBytes(new File(filePath).toPath());
+            byte[] fileBytes = java.nio.file.Files.readAllBytes(new File(fileContent).toPath());
             outputStream.write(fileBytes);
         }
 
